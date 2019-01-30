ec2401f69c3f3d7ee1228ce6d4c5b1f9f6afcd3d
Siiriiks <jonnymillington@gmail.com>
2019-01-30 15:32:18 +0000

    Add files via upload

--- r-colour.js
+++ r-colour.js
@@ -3,8 +3,10 @@ function random_colour() {
 	var color = '#';
 	var letters = ['ff598f','fd8a5e','e0e300','01dddd'];
 	color += letters[Math.floor(Math.random() * letters.length)];
-	document.getElementById('r-colour').style.background = color;
-	document.getElementById('r-colour1').style.background = color;
+	document.getElementById('r-colour-b').style.background = color;
+	document.getElementById('r-colour1-b').style.background = color;
+	document.getElementById('r-colour').style.color = color;
+	document.getElementById('r-colour1').style.color = color;
 	document.getElementById('r-colour2').style.color = color;
 	document.getElementById('r-colour3').style.color = color;
 	document.getElementById('r-colour4').style.color = color;
